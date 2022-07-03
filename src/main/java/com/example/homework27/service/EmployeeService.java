484ba39259d9e5faa22bddb01c5aa302ac242ede
Webkonditer <webkonditer@yandex.ru>
2022-07-03 21:27:49 +0700

    done

--- src/main/java/com/example/homework27/service/EmployeeService.java
+++ src/main/java/com/example/homework27/service/EmployeeService.java
@@ -1,13 +1,11 @@
 package com.example.homework27.service;
-
 import com.example.homework27.model.Employee;
-
-import java.util.List;
+import java.util.Map;
 
 public interface EmployeeService {
 
     Employee addNewEmployee(String lastName, String firstName);
     Employee findEmployee(String lastName, String firstName);
     Employee removeEmployee(String lastName, String firstName);
-    List<Employee> list();
+    Map<String, Employee> list();
 }
